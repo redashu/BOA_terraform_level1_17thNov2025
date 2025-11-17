c0b4060b0ac20cd34e200ab9a5d147a38dbf67e9
Ashutoshh <ashutoshh@linux.com>
2025-11-17 23:40:21 +0530

    updates

--- day1/hello.tf
+++ day1/hello.tf
@@ -1,7 +1,17 @@
 provider "aws" {
 
-   access_key = ""
-   secret_key = ""
+#    access_key = ""
+#    secret_key = ""
    region = "us-east-1"
   
+}
+
+# creating ec2 machine in NV region 
+
+resource "aws_instance" "example" {
+    ami = "ami-0cae6d6fe6048ca2c"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "ashutoshh-vm1"
+    }
 }
